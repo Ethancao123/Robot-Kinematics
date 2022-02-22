594e00d3a893f43533705996348aa99cbab5ee36
Ethancao123 <ethan.cao123456@gmail.com>
2022-02-22 14:54:04 -0800

    worky

--- carPython/DriveShapes.py
+++ carPython/DriveShapes.py
@@ -6,12 +6,14 @@ import numpy as np
 import mecanum_py_by_wheel_encodeSpeed as drive
 
 s = sched.scheduler(time.time, time.sleep)
-point1 = [1, 1]
-point2 = [3, 1]
+point1 = [0, 0]
+point2 = [30, 30]
 point3 = [3, 3]
 point4 = [1, 3]
-IntervalTime = 0.25
-travel_speed = 40
+Path_Points = [[0,0]]
+IntervalTime = 0
+travel_speed = 50
+speed_mult = 1
 
 
 def length(v):
@@ -44,7 +46,7 @@ def path(start_point, end_point, carryover=0):
         for t in range(int((distance-carryover)/travel_speed) + 1):
             x_coord = start_point[0]
             y_coord = distance_covered * moving_vertical + start_point[1]
-            display_point(x_coord, y_coord)
+            # display_point(x_coord, y_coord)
             distance_covered += travel_speed
     else:
         slope = (end_point[1] - start_point[1])/(abs(end_point[0] - start_point[0]))
@@ -52,26 +54,29 @@ def path(start_point, end_point, carryover=0):
         for t in range(int((distance-carryover)/travel_speed) + 1):
             x_coord = math.cos(theta) * distance_covered * moving_horizontal + start_point[0]
             y_coord = math.sin(theta) * distance_covered + start_point[1]
-            display_point(x_coord, y_coord)
+            # display_point(x_coord, y_coord)
             distance_covered += travel_speed
     return distance % travel_speed
 
 def path(end_point, carryover=0):
     # calculate the distance between the start and end points
     start_point = [0,0]
+    if(end_point == [0,0]):
+        return
     angle = math.atan2(end_point[1], end_point[0])
     distance = math.sqrt(math.pow(end_point[1] - start_point[1], 2) + math.pow(end_point[0] - start_point[0], 2))
     distance_covered = carryover
     t = distance / travel_speed
     # determines if the lines grows in the positive or negative y direction
     forward_speed = np.sin(angle) * distance / t
+    print(forward_speed)
     horizontal_speed = np.cos(angle) * distance / t
+    print(horizontal_speed)
     drive.fl.move(forward_speed + horizontal_speed)
     drive.fr.move(forward_speed - horizontal_speed)
     drive.rl.move(forward_speed - horizontal_speed)
     drive.rr.move(forward_speed + horizontal_speed)
-    time.sleep(t)
-    drive.coastAll(1)
+    time.sleep(t/100)
 
 def display_point(x_coord, y_coord):
     time.sleep(1)
@@ -88,7 +93,8 @@ def semi_circle(p1, p2, carryover=0):    # draws a counter-clockwise half circle
     for t in range(int(np.pi / arc_measure)):
         x_coord = np.cos(carryover_angle + initial_angle + (t + 1) * arc_measure) * radius + center[0]
         y_coord = np.sin(carryover_angle + initial_angle + (t + 1) * arc_measure) * radius + center[1]
-        display_point(x_coord, y_coord)
+        # display_point(x_coord, y_coord)
+        Path_Points.add([x_coord, y_coord])
     return radius * np.pi % travel_speed
 
 
@@ -99,13 +105,14 @@ def draw_circle():
     vector = [prev_point[0] - center[0], prev_point[1] - center[1]]
     initial_angle = np.arctan2(vector[1], vector[0])
     print(initial_angle*180/np.pi)
-    arc_measure = travel_speed / radius    # in radians
-    display_point(prev_point[0], prev_point[1])
+    arc_measure = travel_speed / 100 / radius    # in radians
+    # display_point(prev_point[0], prev_point[1])
     for t in range(int(2 * np.pi / arc_measure)):
         x_coord = np.cos(initial_angle + (t + 1) * arc_measure) * radius + center[0]
         y_coord = np.sin(initial_angle + (t + 1) * arc_measure) * radius + center[1]
         next_point = [x_coord, y_coord]
-        display_point(x_coord, y_coord)
+        # display_point(x_coord, y_coord)
+        Path_Points.append([x_coord * speed_mult, y_coord * speed_mult])
         prev_point = next_point
 
 
@@ -125,7 +132,11 @@ def draw_loop():
 
 
 def main():
-    path([300,200])
+    draw_circle()
+    print(Path_Points)
+    for x in Path_Points:
+        path(x)
+    drive.coastAll(1)
 
 if __name__ == '__main__':
     try:
