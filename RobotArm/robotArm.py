c5759755c574be0f931a9642a701a07df4f95582
Ethancao123 <ethan.cao123456@gmail.com>
2022-04-22 11:04:43 -0700

    update things idk man

--- RobotArm/robotArm.py
+++ RobotArm/robotArm.py
@@ -1,19 +1,25 @@
+import time
 import numpy as np
 import Matrices.frame as fr
+from board import SCL,SDA
+import busio
 from adafruit_pca9685 import PCA9685
+import RPi.GPIO as GPIO #control through GPIO pins
 
 # adafruit forces GPIO.setmode(GPIO.BCM)
 GPIO.setmode(GPIO.BCM)
 #I2C for PCS9685 and Gyro
 # create i2c bus interface to access PCA9685, for example
 i2c = busio.I2C(SCL, SDA)    #busio.I2C(board.SCL, board.SDA) create i2c bus
-pca = PCA9685(i2c)           #adafruit_pca9685.PCA9685(i2c)   instance PCA9685 on bus
+pca = PCA9685(i2c, address = 0x41)           #adafruit_pca9685.PCA9685(i2c)   instance PCA9685 on bus
 pca.frequency = 1000 #set pwm clock in Hz (debug 60 was 1000)
 # usage: pwm_channel = pca.channels[0] instance example
 #        pwm_channel.duty_cycle = speed (0 .. 100)  speed example
 
 PWMOEN = 4 #pin 7 #PCA9685 OEn pin
 pwmOEn = GPIO.setup(PWMOEN, GPIO.OUT)  # enable PCA outputs
+
+R1 = pca.channels[0]
 
 #equivalent of Arduino map()
 def valmap(value, istart, istop, ostart, ostop): 
@@ -26,5 +32,10 @@ def getPWMPer(value):
 
 while True:
     for i in range(100):
-        pca.channels[0].duty_cycle(getPWMPer(i))
-        sleep(0.01)
+        R1.duty_cycle = getPWMPer(i)
+        time.sleep(0.01)
+        print(i)
+    for i in range(100):
+        R1.duty_cycle = getPWMPer(100-i)
+        time.sleep(0.01)
+        print(i)
